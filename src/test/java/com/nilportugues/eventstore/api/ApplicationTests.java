aaf1f9bc4bf4212cc25e7d7803aea2f9f6533a44
Nil Portugués Calderó <nil@trovit.com>
2019-03-20 16:34:52 +0100

    remove .idea

--- src/test/java/com/nilportugues/eventstore/api/ApplicationTests.java
+++ src/test/java/com/nilportugues/eventstore/api/ApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
-public class ItubeApplicationTests {
+public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {

1ac96a1b3b7cbaa3bab869c6ba24dec3d9e28399
Nil Portugués Calderó <nil@trovit.com>
2019-03-20 16:32:16 +0100

    first commit

---
+++ src/main/java/com/nilportugues/eventstore/api/Application.java
@@ -0,0 +1,45 @@
+package com.nilportugues.eventstore.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer  {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+
+
+
+	}
+	
+	@Configuration
+	public static class WebConfig extends WebMvcConfigurerAdapter {
+
+		@Override
+		public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
+			configurer.setDefaultTimeout(-1);
+			configurer.setTaskExecutor(asyncTaskExecutor());
+		}
+		
+		@Bean
+		public AsyncTaskExecutor asyncTaskExecutor() {
+			return new SimpleAsyncTaskExecutor("async");
+		}
+		
+	}
+
+}
---
+++ src/main/java/com/nilportugues/eventstore/api/controller/IndexController.java
@@ -0,0 +1,74 @@
+package com.nilportugues.eventstore.api.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping("/")
+	public StreamingResponseBody welcome() throws Exception {
+        return out -> {
+            URL url = new URL("http://localhost:8090/infinite-json");
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            try {
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+                reader.lines().forEach(line -> {
+                    try {
+                        out.write((line+"\n").getBytes());
+                        out.flush();
+                    } catch (IOException ignored) {
+                        ignored.printStackTrace();
+                        return;
+                    }
+                });
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            finally {
+                urlConnection.disconnect();
+            }
+
+        };
+	}
+
+
+	@RequestMapping(method = RequestMethod.GET, value = "/infinite-json", produces = "application/json")
+	public StreamingResponseBody handleRequest () {
+
+	    TimeZone tz = TimeZone.getTimeZone("UTC");
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
+        df.setTimeZone(tz);
+
+		return out -> {
+            for (int i = 0; i < 100; i++) {
+                String nowAsISO = String.valueOf(new Date().toInstant().getEpochSecond());
+
+                out.write(("{\"id\": "+i+", \"hello\": \""+nowAsISO+"\"}\n").getBytes());
+                out.flush();
+
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+	}
+
+}
